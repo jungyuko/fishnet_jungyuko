1d94402a419687d680de48dae61005ac5a1ceca3
Jun Gyu <43873104+jungyuko@users.noreply.github.com>
2022-02-12 18:16:39 +0900

    train_test code modify
    top 5 accuracy 추가

--- eval.py
+++ eval.py
@@ -1,13 +1,17 @@
 import argparse
 
-import numpy as np
 import torch
-
+from tqdm import tqdm
 from models.fishnet import build_fishnet
 from utils import prepare_dataloader
 
-parser = argparse.ArgumentParser(description='FishNet')
 
+## Device
+device = torch.device("cuda:0")
+print(device)
+
+
+parser = argparse.ArgumentParser(description='FishNet')
 parser.add_argument("--img_size", type=int, default=32, help="img_size")
 
 # Model Parameters
@@ -17,54 +21,67 @@ parser.add_argument("--n_head", type=int, default=3, help="number of head module
 parser.add_argument("--in_channel", type=int, default=64, help="dimension of channels")
 parser.add_argument("--out_channel", type=int, default=64, help="dimension of channels")
 
+# Training Parameters
+parser.add_argument("--batch_size", type=int, default=1, help="number of batch")
+
+
 args = parser.parse_args()
 print(args)
 
-## Device
-device = torch.device("cuda:0")
-print(device)
-
-
 ## Dataloader
-_,_, _, _, test_dataset, test_loader = prepare_dataloader(img_size=args.img_size)
+_,_, _, _, test_dataset, test_loader = prepare_dataloader(
+    batch_size=args.batch_size,
+    img_size=args.img_size)
 
-print('length of batch size, test data: {}'.format(len(test_loader)))
+print(len(test_loader))
 
-## model
+
+## Model
 model = build_fishnet(args)
 model = model.to(device)
+print(model)
 
 
-## load params
-model.load_state_dict(torch.load('./utils_model.pth'))
+## load model parameter
+model.load_state_dict(torch.load('./utils_model2.pth'))
 
 
+# test
 def test(model, device, test_loader):
-
-    # Test Phase
+    
     model.eval()
 
-    correct = 0
-    count   = 0
-    for batch_num, data in enumerate(test_loader):
+    total_top1_test_acc = 0
+    total_top5_test_acc = 0
+    
+    for batch_num, data in tqdm(enumerate(test_loader)):
         images, labels = data
+
         images = images.to(device)
         labels = labels.to(device)
-
+        
         with torch.no_grad():
             output = model(images)
-        pred_labels = np.argmax(output.cpu().detach().numpy())
+        
+        # acc
+        _, pred = output.data.topk(5, 1, True, True)
+        pred = pred.t()
+        
+        pred_labels = pred.eq(labels.view(1,-1).expand_as(pred))
 
-        if labels == pred_labels:
-            correct += 1
-            count   += 1
-        else:
-            count += 1
-        if batch_num % 250 == 0:
-            print('{}/{}, test_acc: {:.4f}%'.format(batch_num, len(test_loader),(correct/count)*100))
+        top1 = pred_labels[:1].reshape(-1).float().sum(0, keepdim=True)
+        top5 = pred_labels[:5].reshape(-1).float().sum(0, keepdim=True)
+        top1_acc = (top1/args.batch_size)*100
+        top5_acc = (top5/args.batch_size)*100
+        
+        total_top1_test_acc += top1_acc
+        total_top5_test_acc += top5_acc
     
-    print('acc: {:.4f}%'.format((correct/count)*100))
+    
+    print("top 1 accuracy: {:.4f}%, top 5 accuracy: {:.4f}%".format(
+        total_top1_test_acc.item()/len(test_loader), total_top5_test_acc.item()/len(test_loader)))
 
 
+# run code
 if __name__ == "__main__":
     test(model, device, test_loader)
--- train.py
+++ train.py
@@ -1,29 +1,29 @@
 import argparse
 import logging
 
-import numpy as np
-
 import torch
 import torch.nn as nn
+
+from torch.optim import lr_scheduler
 
 from models.fishnet import build_fishnet
 from utils import prepare_dataloader, config_logging, BestSaver, AverageMeter
 
-from matplotlib import pyplot as plt
 
 from tqdm import tqdm
 
 
+## Use GPU
 device = torch.device("cuda:0")
 print(device)
 
 
+## parameter setting
 parser = argparse.ArgumentParser(description='FishNet')
 
 parser.add_argument("--img_size", type=int, default=32, help="img_size")
 
 # Model Parameters
-parser.add_argument("--batch_size", type=int, default=256, help="number of batch")
 parser.add_argument("--n_tail", type=int, default=3, help="number of tail modules")
 parser.add_argument("--n_body", type=int, default=3, help="number of body modules")
 parser.add_argument("--n_head", type=int, default=3, help="number of head modules")
@@ -31,16 +31,17 @@ parser.add_argument("--in_channel", type=int, default=64, help="dimension of cha
 parser.add_argument("--out_channel", type=int, default=64, help="dimension of channels")
 
 # Training Parameters
+parser.add_argument("--batch_size", type=int, default=256, help="number of batch")
 parser.add_argument("--lr", type=float, default=1e-1, help="initial learning rate")
 parser.add_argument("--wd", type=float, default=1e-4, help="initial learning rate")
 parser.add_argument("--momentum", type=float, default=0.9, help="initial learning rate")
-parser.add_argument("--epochs", type=int, default=50, help="number of max epochs")
+parser.add_argument("--epochs", type=int, default=100, help="number of max epochs")
+parser.add_argument("--lrd", type=float, default=1e-1, help="multiplicative factor of learning rate decay")
+parser.add_argument("--step_size", type=int, default=30, help="period of learning rate decay")
 
-# name
-parser.add_argument("--comment", type=str, default='model', help="save pth name")
-
-args = parser.parse_args('')
-print(args)
+# wandb
+parser.add_argument("--comment", type=str, default='model_params', help="save pth name")
+args = parser.parse_args()
 
 
 comment = args.comment
@@ -50,25 +51,31 @@ logging.info("arguments: {}".format(args))
 
 
 ## Dataloader
-train_dataset, train_loader, valid_dataset, valid_loader, _, _ = prepare_dataloader(img_size=args.img_size)
+train_dataset, train_loader, valid_dataset, valid_loader, _, _ = prepare_dataloader(img_size=args.img_size, batch_size=args.batch_size)
 
-print('length of batch size, train data: {}'.format(len(train_loader)))
-print('length of batch size, valid data: {}'.format(len(valid_loader)))
+print(len(train_loader))
+print(len(valid_loader))
+
 
 ## model
 model = build_fishnet(args)
 model = model.to(device)
+print(model)
 
 
+## Train
 total_train_losses = []
-total_train_acc = []
-total_valid_acc = []
+total_top1_train_accs = []
+total_top5_train_accs = []
+total_top1_valid_accs = []
+total_top5_valid_accs = []
 
 def train(model, device, train_loader, valid_loader):
     
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, 
                                 weight_decay=args.wd, momentum=args.momentum)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.lrd)
 
     epochs = args.epochs
     saver = BestSaver(comment)
@@ -76,69 +83,89 @@ def train(model, device, train_loader, valid_loader):
     for epoch in tqdm(range(1, epochs+1)):
         logging.info("Train Phase, Epoch: {}/{}".format(epoch, epochs))
         train_losses = AverageMeter()
+        scheduler.step()
         
         # Train Phase
         model.train()
 
-        correct = 0
-        count   = 0
+        total_top1_train_acc = 0
+        total_top5_train_acc = 0
         for batch_num, data in enumerate(train_loader):
             images, labels = data
             images = images.to(device)
             labels = labels.to(device)
             output = model(images)
-            pred_labels = np.argmax(output.cpu().detach().numpy(), axis=1)
-
+        
             # loss
             train_loss = criterion(output, labels)
             train_losses.update(train_loss.item(), images.shape[0])
 
+            # acc
+            _, pred = output.data.topk(5, 1, True, True)
+            pred = pred.t()
+            
+            pred_labels = pred.eq(labels.view(1,-1).expand_as(pred))
 
-            for i in range(len(labels)):
-                if labels[i] == pred_labels[i]:
-                    correct += 1
-                    count   += 1
-                else:
-                    count += 1
-
+            top1 = pred_labels[:1].reshape(-1).float().sum(0, keepdim=True)
+            top5 = pred_labels[:5].reshape(-1).float().sum(0, keepdim=True)
+            top1_acc = (top1/args.batch_size)*100
+            top5_acc = (top5/args.batch_size)*100
+            
+            total_top1_train_acc += top1_acc
+            total_top5_train_acc += top5_acc
+            
+            # backward
             optimizer.zero_grad()
             train_loss.backward()
             optimizer.step()
 
             if batch_num % 20 == 0:
                 logging.info(
-                    "[{}/{}] # {}/{} loss: {:.4f}".format(epoch, epochs, batch_num, len(train_loader), train_losses.val)
+                    "[{}/{}] # {}/{} loss: {:.4f}".format(
+                        epoch, epochs, batch_num, len(train_loader), train_losses.val)
                 )
-        
-        logging.info("Train_loss: {:.4f}, Train_acc: {:.4f}%".format(train_losses.avg, (correct/count)*100))
+        logging.info("Train_loss: {:.4f}, top1_train_acc: {:.4f}%, top5_train_acc: {:.4f}%".format(
+            train_losses.avg, total_top1_train_acc.item()/len(train_loader), total_top5_train_acc.item()/len(train_loader)))
         total_train_losses.append(train_losses.avg)
-        total_train_acc.append(correct/count)
+        total_top1_train_accs.append(total_top1_train_acc.item()/len(train_loader))
+        total_top5_train_accs.append(total_top5_train_acc.item()/len(train_loader))
+
 
         # Valid Phase
         model.eval()
 
-        correct = 0
-        count   = 0
+        total_top1_valid_acc = 0
+        total_top5_valid_acc = 0
+        
         for batch_num, data in enumerate(valid_loader):
-            images, gt_labels = data
+            images, labels = data
 
-            images    = images.to(device)
-            gt_labels = gt_labels.to(device)
+            images = images.to(device)
+            labels = labels.to(device)
             
             with torch.no_grad():
                 output = model(images)
-            pred_labels = np.argmax(output.cpu().detach().numpy(), axis=1)
+            # acc
+            _, pred = output.data.topk(5, 1, True, True)
+            pred = pred.t()
             
-            for i in range(len(gt_labels)):
-                if gt_labels[i] == pred_labels[i]:
-                    correct += 1
-                    count   += 1
-                else:
-                    count += 1
+            pred_labels = pred.eq(labels.view(1,-1).expand_as(pred))
 
-        logging.info('valid_acc: {:.4f}%'.format((correct/count)*100))
-        total_valid_acc.append(correct/count)
-        saver.save(correct/count, model.state_dict())    
+            top1 = pred_labels[:1].reshape(-1).float().sum(0, keepdim=True)
+            top5 = pred_labels[:5].reshape(-1).float().sum(0, keepdim=True)
+            top1_acc = (top1/args.batch_size)*100
+            top5_acc = (top5/args.batch_size)*100
+            
+            total_top1_valid_acc += top1_acc
+            total_top5_valid_acc += top5_acc
 
+        logging.info('top1_valid_acc: {:.4f}%, top5_valid_acc: {:.4f}'.format(
+            total_top1_valid_acc.item()/len(valid_loader), total_top5_valid_acc.item()/len(valid_loader)))
+        total_top1_valid_accs.append(total_top1_valid_acc.item()/len(valid_loader))
+        total_top5_valid_accs.append(total_top5_valid_acc.item()/len(valid_loader))
+        # save best model
+        saver.save(total_top1_valid_acc.item()/len(valid_loader), model.state_dict())    
+
+# run code
 if __name__ == "__main__":
-    train(model, device, train_loader, valid_loader)   
+    train(model, device, train_loader, valid_loader)
